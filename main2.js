b1ca8aea9b44ef8bc47af1641d2d8cc1a9c98bd0
Jefferson Suarez Sayin <sayinjefferson@users.noreply.github.com>
2024-07-03 15:53:46 +0800

    main2.js

--- main2.js
+++ main2.js
@@ -1,47 +1,64 @@
-$("#messageState").on("change", (x) => {
-    $(".message").removeClass("openNor").removeClass("closeNor");
-    if ($("#messageState").is(":checked")) {
-      $(".message")
-        .removeClass("closed")
-        .removeClass("no-anim")
-        .addClass("openNor");
-      $(".heart")
-        .removeClass("closeHer")
-        .removeClass("openedHer")
-        .addClass("openHer");
-      $(".container").stop().animate({ backgroundColor: "#f48fb1" }, 2000);
-      console.log("Abrindo");
-    } else {
-      $(".message").removeClass("no-anim").addClass("closeNor");
-      $(".heart")
-        .removeClass("openHer")
-        .removeClass("openedHer")
-        .addClass("closeHer");
-      $(".container").stop().animate({ backgroundColor: "#fce4ec" }, 2000);
-      console.log("fechando");
-    }
-  });
+const openBtn = document.querySelector(".card-container");
+const to = document.querySelector(".to");
+const my = document.querySelector(".my");
+const from = document.querySelector(".from");
+
+const toArr = [
+  // "Purple Donkey Ears",
+  // "Glow Stick",
+  // "Luscious LipBalm",
+  // "Reticulated Python",
+  // "HTML",
+  // "Palm Tree",
+  // "Kitchen Table",
+  // "Javascript",
+  // "Fireworks",
+  // "Infinite Pikachu",
+  // "Fabulous Artichoke"
+];
+const myArr = [
+  // "Insomnia",
+  // "Cherry Tomato",
+  // "Coffee Table",
+  // "Pink Fairy Armadillo",
+  // "Coral Reef",
+  // "Cozy Armchair",
+  // "CSS",
+  // "Motorcycle",
+  // "Shiny Disco Ball",
+  // " Pygmy Goat",
+  // "Space Hopper"
+];
+const fromArr = [
+  // "Disco Dancer",
+  // "Raving Pigeon",
+  // "Cocktail Umbrella",
+  // "Speeding Chicken",
+  // "Can Opener",
+  // "Cunning Fox",
+  // "Biscuit Barrel",
+  // "Sneezing Panda",
+  // "Complicated Monkey",
+  // "Menacing Mongoose"
+];
+
+let tooMsg = toArr[Math.floor(Math.random() * toArr.length)];
+let myMsg = myArr[Math.floor(Math.random() * myArr.length)];
+let fromMsg = fromArr[Math.floor(Math.random() * fromArr.length)];
+
+const generateCard = () => {
+  to.innerHTML = `Happy 3rd Year Anniversary MyLove! Unang una sa lahat nagpapasalamat ako sa Diyos dahil hindi nya tayo pinapabayaan sa kahit anong pagsubok na dumaan sating dalawa. 
   
-  $(".message").on(
-    "webkitAnimationEnd oanimationend msAnimationEnd animationend",
-    function (e) {
-      console.log("Animation End");
-      if ($(".message").hasClass("closeNor")) $(".message").addClass("closed");
-      $(".message")
-        .removeClass("openNor")
-        .removeClass("closeNor")
-        .addClass("no-anim");
-    }
-  );
+  Sa ating mga susunod na paglalakbay, maraming pagsubok man, tandaan mo na nandito lang ako palagi para sayo. Maraming salamat sa pagmamahal, pang-unawa, at suporta na ibinibigay mo sa akin.
   
-  $(".heart").on(
-    "webkitAnimationEnd oanimationend msAnimationEnd animationend",
-    function (e) {
-      console.log("Animation End");
-      if (!$(".heart").hasClass("closeHer"))
-        $(".heart").addClass("openedHer").addClass("beating");
-      else $(".heart").addClass("no-anim").removeClass("beating");
-      $(".heart").removeClass("openHer").removeClass("closeHer");
-    }
-  );
+  Nawa'y patnubayan tayo ng panginoon sa ating pagsasama at tulungan nya tayo sa ating mga susunod na taon. I LOVE YOU LOVE!
   
+  HAPPY ANNIVERSARY MYLOVE.`;
+  my.innerHTML = `"You are my LOVE the one that takes away all my SADNESS, the one that gives me FEAR when we're fighting, the one that gives me ANGER when you say everything you want to say and not listening to me, and lastly the one that gives me JOY when we're together.`;
+  // from.innerHTML = ` <br> <b></b>`;
+};
+
+openBtn.onclick = function () {
+  document.body.classList.toggle("open");
+  generateCard();
+};
